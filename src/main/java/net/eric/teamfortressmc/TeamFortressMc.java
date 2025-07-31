300aeef6b4e76563e387770786d440a536197566
Bluedogman <goeric09@gmail.com>
2025-07-31 14:21:15 -0500

    Working on adding loot_tables still not an official release

--- src/main/java/net/eric/teamfortressmc/TeamFortressMc.java
+++ src/main/java/net/eric/teamfortressmc/TeamFortressMc.java
@@ -1,6 +1,10 @@
 package net.eric.teamfortressmc;
 
 import com.mojang.logging.LogUtils;
+import net.eric.teamfortressmc.block.ModBlocks;
+import net.eric.teamfortressmc.item.ModCreativeModeTabs;
+import net.eric.teamfortressmc.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -8,24 +12,29 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TeamFortessMc.MOD_ID)
-public class TeamFortessMc
+@Mod(TeamFortressMc.MOD_ID)
+public class TeamFortressMc
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "teamfortessmc"; //LOOK HERE FOR MOD_ID
+    public static final String MOD_ID = "teamfortressmc"; //LOOK HERE FOR MOD_ID
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public TeamFortessMc(FMLJavaModLoadingContext context)
+    public TeamFortressMc(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
+
+        ModBlocks.register(modEventBus);
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -42,6 +51,10 @@ public class TeamFortessMc
 
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        //if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+        //    event.accept(ModItems.SAPPHIRE);
+        //    event.accept(ModItems.RAW_SAPPHIRE);
+        //}
     }
 
     @SubscribeEvent
